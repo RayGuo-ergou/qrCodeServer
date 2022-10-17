e8d4053f89304413a7bfdc6bd43d25eeb3e3831d
Ray Guo <ma2808203259@hotmail.com>
2022-10-17 17:48:19 +1100

    fix: fix the logout clear cookie issue

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,16 +1,9 @@
 const logout = (req, res) => {
-    // FIXME: because my backend and frontend not in same domain
-    // so use this stupid way around to logout
-    // in future, if have time, will try to fix this
     res.status(200)
-        .cookie('token', 'logout', {
+        .clearCookie('token', {
             sameSite: 'none',
             secure: true,
         })
-        .clearCookie('token')
         .json({ message: 'Logout successful' });
-    // remove cookie from browser
-    // res.clearCookie('token').json({ message: 'Logout successful' });
 };
-
 module.exports = logout;
